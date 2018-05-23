b71f59fca2ab3e7e3719c08ad9a66091ea33c79e
dermio <thisdjay@gmail.com>
2018-05-22 22:13:35 -0700

    Created initial server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/api/*", function (req, res) {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+module.exports = {app};
