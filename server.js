8f28614d2b9d87972395d681def80732998de00b
dermio <thisdjay@gmail.com>
2018-05-24 11:18:41 -0700

    Configure cors to use client-origin

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const { CLIENT_ORIGIN } = require("./config");
 
 const PORT = process.env.PORT || 8080;
 
+app.use(
+  cors( {origin: CLIENT_ORIGIN} )
+);
+
 app.get("/api/*", function (req, res) {
   res.json({ ok: true });
 });
