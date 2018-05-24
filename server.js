8f11460cf9ed7bc739e46825c89c42b4691bcfe6
dermio <thisdjay@gmail.com>
2018-05-24 11:09:30 -0700

    Required config file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
+
 const PORT = process.env.PORT || 8080;
 
 app.get("/api/*", function (req, res) {
