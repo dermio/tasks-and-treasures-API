683fdbd0ab2c45f8e15b31bfc344b6068f1fc06d
dermio <thisdjay@gmail.com>
2018-05-29 11:31:52 -0700

    Setup variables for the Port and MongoDB URL

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-// Empty config file for now
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       "mongodb://localhost/tasks-and-treasures-app";
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
+                            "mongodb://localhost/tasks-and-treasures-app";
+exports.PORT = process.env.PORT || 8080;
