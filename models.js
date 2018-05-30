d16e644cf55a11d3dbd4ca0d151c6be3fe9814a1
dermio <thisdjay@gmail.com>
2018-05-30 14:25:31 -0700

    Created schemas for parent and child users

--- models.js
+++ models.js
@@ -10,5 +10,20 @@ const prizeSchema = mongoose.Schema({
   familyCode: {type: String, required: true}
 });
 
+const parentUser = mongoose.Schema({
+  role: {type: String, required: true},
+  userName: {type: String, required: true},
+  password: {type: String, required: true}, // password stored as HASH
+  name: {type: String, required: true},
+  familyCode: {type: String, required: true}
+});
 
-
+const childUser = mongoose.Schema({
+  role: {type: String, required: true},
+  userName: {type: String, required: true},
+  password: {type: String, required: true}, // password stored as HASH
+  name: {type: String, required: true},
+  familyCode: {type: String, required: true},
+  completed_tasks: [String] // not required, list might be empty
+  approved: {type: Boolean, required: true}
+});
