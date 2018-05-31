b4d1eef89a07627343d9946a0df26425b7c329ff
dermio <thisdjay@gmail.com>
2018-05-30 18:41:15 -0700

    Changed database URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       "mongodb://localhost/tasks-and-treasures-app";
+                       "mongodb://localhost/taskTreasureDB";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                            "mongodb://localhost/test-tasks-and-treasures-app";
+                            "mongodb://localhost/test-taskTreasureDB";
 exports.PORT = process.env.PORT || 8080;
