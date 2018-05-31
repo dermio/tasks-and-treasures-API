21b42b45ffc86d6fbfb3aeb5315cd04412517313
dermio <thisdjay@gmail.com>
2018-05-31 06:37:56 -0700

    Edited database URL to match remote URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       "mongodb://localhost/taskTreasureDB";
+                       "mongodb://localhost/task-treasure-db";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-                            "mongodb://localhost/test-taskTreasureDB";
+                            "mongodb://localhost/test-task-treasure-db";
 exports.PORT = process.env.PORT || 8080;
