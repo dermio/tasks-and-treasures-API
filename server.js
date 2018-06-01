33adbd6671f374160458dad433fc5daf169032c6
dermio <thisdjay@gmail.com>
2018-06-01 14:22:36 -0700

    Removed app.listen() from global scope

--- server.js
+++ server.js
@@ -19,15 +19,6 @@ app.get("/api/*", function (req, res) {
 });
 
 
-/* old
-app.listen(PORT, (req, res) => {
-  console.log(`Listening on port ${PORT}`);
-});
-
-module.exports = {app};
-*/
-
-
 /* closeServer needs access to a server object, but that only
 gets created when `runServer` runs, so we declare `server` here
 and then assign a value to it in run */
