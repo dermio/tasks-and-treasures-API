95f23ff756da8c1add200e567daa1a308a4db280
dermio <thisdjay@gmail.com>
2018-06-02 16:04:09 -0700

    Removed schemas for users and prizes

--- models.js
+++ models.js
@@ -5,30 +5,6 @@ const taskSchema = mongoose.Schema({
   familyCode: {type: String, required: true}
 });
 
-const prizeSchema = mongoose.Schema({
-  prizeName: {type: String, required: true},
-  familyCode: {type: String, required: true}
-});
-
-const parentUser = mongoose.Schema({
-  role: {type: String, required: true},
-  userName: {type: String, required: true},
-  password: {type: String, required: true}, // password stored as HASH
-  name: {type: String, required: true},
-  familyCode: {type: String, required: true}
-});
-
-const childUser = mongoose.Schema({
-  role: {type: String, required: true},
-  userName: {type: String, required: true},
-  password: {type: String, required: true}, // password stored as HASH
-  name: {type: String, required: true},
-  familyCode: {type: String, required: true},
-  completed_tasks: [String], // not required, list might be empty
-  approved: {type: Boolean, required: true}
-});
-
-
 taskSchema.methods.serialize = function () {
   return {
       id: this.id,
