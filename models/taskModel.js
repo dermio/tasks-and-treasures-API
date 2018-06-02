da4447b11f0543e65f7a6b8993295ce36f288509
dermio <thisdjay@gmail.com>
2018-06-02 16:44:31 -0700

    Created taskModel file

---
+++ models/taskModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskSchema = mongoose.Schema({
+  taskName: {type: String, required: true},
+  familyCode: {type: String, required: true}
+});
+
+taskSchema.methods.serialize = function () {
+  return {
+      id: this.id,
+      taskName: this.taskName,
+      familyCode: this.familyCode
+  };
+};
+
+const Task = mongoose.model("tasks", taskSchema);
+
+module.exports = { Task };
