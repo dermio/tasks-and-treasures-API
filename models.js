177c28f2172dbf974cb6d36b532093fe92326e32
dermio <thisdjay@gmail.com>
2018-06-02 16:41:05 -0700

    Commented out code in models.js

--- models.js
+++ models.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const taskSchema = mongoose.Schema({
-  taskName: {type: String, required: true},
-  familyCode: {type: String, required: true}
-});
+// const taskSchema = mongoose.Schema({
+//   taskName: {type: String, required: true},
+//   familyCode: {type: String, required: true}
+// });
 
-taskSchema.methods.serialize = function () {
-  return {
-      id: this.id,
-      taskName: this.taskName,
-      familyCode: this.familyCode
-  };
-};
+// taskSchema.methods.serialize = function () {
+//   return {
+//       id: this.id,
+//       taskName: this.taskName,
+//       familyCode: this.familyCode
+//   };
+// };
 
-const Task = mongoose.model("tasks", taskSchema);
+// const Task = mongoose.model("tasks", taskSchema);
 
-module.exports = { Task };
+// module.exports = { Task };
