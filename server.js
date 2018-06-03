067037c307d969c7dacbe8bb500747a32139cf3d
dermio <thisdjay@gmail.com>
2018-06-02 17:14:07 -0700

    Removed taskModel requirement

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const cors = require("cors");
 const { CLIENT_ORIGIN, PORT, DATABASE_URL } = require("./config");
-const { Task } = require("./models/taskModel");
 
 const tasksRouter = require("./routers/tasksRouter");
 
