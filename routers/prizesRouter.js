b24d70317119d562ffcbbdbd2394f77d4c58509e
dermio <thisdjay@gmail.com>
2018-06-02 21:15:24 -0700

    Created route for GET request for prizes

--- routers/prizesRouter.js
+++ routers/prizesRouter.js
@@ -6,3 +6,22 @@ const jsonParser = bodyParser.json();
 
 const { Prize } = require("../models/prizeModel");
 
+
+// GET prize (prizes), for Parent and Child User with particular family code.
+router.get("/:familyCode", (req, res) => {
+  Prize.find({ familyCode: req.params.familyCode })
+       .then((prizes) => {
+         res.json(prizes.map(prize => prize.serialize()));
+         /* either or
+         res.json({
+           prizes: prizes.map(prize => prize.serialize())
+         });
+         */
+       })
+       .catch(err =>{
+         console.error(err);
+         res.status(500).json({error: "Internal server error"});
+       });
+});
+
+module.exports = router;
