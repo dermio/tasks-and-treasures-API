afd19d6bc587f056db18a095c4681af33bd0dd00
dermio <thisdjay@gmail.com>
2018-06-02 23:26:16 -0700

    Imported packages, files, and tearDownDb() for prizesRouter

---
+++ test/test-prizesRouter.js
@@ -0,0 +1,23 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const mongoose = require("mongoose");
+const faker = require("faker");
+
+const { app, runServer, closeServer } = require("../server");
+const { Prize } = require("../models/prizeModel");
+const { TEST_DATABASE_URL } = require("../config");
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+
+/* this function deletes the entire database.
+we'll call it in an `afterEach` block below
+to ensure data from one test does not stick
+around for next one */
+function tearDownDb() {
+  console.warn('Deleting database');
+  return mongoose.connection.dropDatabase();
+}
+
+
