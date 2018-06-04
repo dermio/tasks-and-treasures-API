e4d51ec9dbf4ab328f9526eccbee325a7180d1f0
dermio <thisdjay@gmail.com>
2018-06-04 16:11:24 -0700

    Added CODE and SECRET variables

--- config.js
+++ config.js
@@ -3,3 +3,8 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                             "mongodb://localhost/test-task-treasure-db";
 exports.PORT = process.env.PORT || 8080;
+
+// Auth
+exports.CODE = process.env.CODE;
+
+exports.SECRET = process.env.SECRET || "keyboard cat";
