ce5c38f18704633841e6ac11db9bcd8866aaac57
dermio <thisdjay@gmail.com>
2018-06-03 21:59:44 -0700

    Required packages and files for usersRouter

---
+++ routers/usersRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const { User } = require("../models/userModel");
+
+
