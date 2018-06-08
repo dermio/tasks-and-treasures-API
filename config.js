7499472424c7bcb1e489f3e5b94476ef9906378c
dermio <thisdjay@gmail.com>
2018-06-08 10:13:07 -0700

    Updated config file with JWT variables

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
                        "mongodb://localhost/task-treasure-db";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                             "mongodb://localhost/test-task-treasure-db";
 exports.PORT = process.env.PORT || 8080;
 
 // Auth
-exports.CODE = process.env.CODE;
-
-exports.SECRET = process.env.SECRET || "keyboard cat";
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
