71a103b99f98f0b5de5cdb4b58af3163d94b4d26
dermio <thisdjay@gmail.com>
2018-06-07 19:08:10 -0700

    Deleted old auth code

--- server.js
+++ server.js
@@ -17,97 +17,10 @@ app.use("/api/prizes", prizesRouter);
 
 
 /****************************
- * Related to Auth
+ * Related to Auth, C.K.
 ****************************/
 
-const morgan = require("morgan");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const expressValidator = require("express-validator");
-const bcrypt = require("bcryptjs");
-const path = require("path");
-const flash = require("connect-flash");
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const jsonParser = bodyParser.json();
-
-// Sets static public folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Bodyparser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-
-// Express session middleware
-app.use(
-	session({
-		secret: SECRET,
-		resave: false,
-		saveUninitialized: false
-	})
-);
-
-// Connect Flash
-app.use(flash());
-
-// Connect Flash messages
-app.use(function(req, res, next) {
-	res.locals.success_msg = req.flash("success_msg");
-	res.locals.error_msg = req.flash("error_msg");
-	res.locals.error = req.flash("error");
-	res.locals.info_msg = req.flash("info_msg");
-	next();
-});
-
-// Express Validator
-const { check, validationResult } = require("express-validator/check");
-const { matchedData, sanitize } = require("express-validator/filter");
-
-app.use(expressValidator());
-
-// Passport Init
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Morgan logging middleware
-app.use(morgan("common"));
-
-// Routers and modules: Chris K comment out
-/* const loginRouter = require("./routers/loginRouter");
-const registerRouter = require("./routers/registerRouter");
-
-app.use("/login", loginRouter);
-app.use("/register", registerRouter); */
-
-// Checks to see if user is authenticated to access protected routes
-function isLoggedIn(req, res, next) {
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated()) {
-		return next();
-	} else {
-		// if they aren't redirect them to the home page
-		res.redirect("/login");
-	}
-}
-
-app.get("/", (req, res) => {
-	res.redirect("/login");
-});
-/* // Login Screen: Chris K comment out
-app.get("/login", (req, res) => {
-	res.sendFile("public/login.html", { root: __dirname });
-});
-// Register
-app.get("/register", (req, res) => {
-	res.sendFile("public/register.html", { root: __dirname });
-});
-app.get("/auth", isLoggedIn, (req, res) => {
-	res.sendFile("public/auth.html", { root: __dirname });
-}); */
 
 /****************************
  * Related to Auth
