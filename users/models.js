780d65711dd107352b3acdf74e19b10fc1cdaaa5
dermio <thisdjay@gmail.com>
2018-06-12 19:05:13 -0700

    Changed default Thinkful user models code with capstone model

--- users/models.js
+++ users/models.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
@@ -36,4 +36,48 @@ UserSchema.statics.hashPassword = function(password) {
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = {User}; */
+
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+  role: {type: String, required: true},
+  username: {type: String, required: true /* , unique: true */},
+  password: {type: String, required: true}, // password stored as HASH
+  name: {type: String, required: true},
+  familyCode: {type: String, required: true},
+  completedTasks: [String], // not required, for Child user
+  approved: {type: Boolean}, // not required, for Child user
+
+  email: {type: String /* , unique: true */, required: true}
+});
+
+userSchema.methods.serialize = function () {
+  return {
+    id: this._id,
+    role: this.role,
+    username: this.username,
+    password: this.password, // Should hashed password be returned?
+    name: this.name,
+    familyCode: this.familyCode,
+    completedTasks: this.completedTasks,
+    approved: this.approved,
+
+    email: this.email
+  };
+};
+
+// Auth code
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model("users", userSchema, "users");
+
+module.exports = { User };
