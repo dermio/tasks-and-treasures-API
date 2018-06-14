2b68c66face3bc05bbc268b9b755fba61bed2e81
dermio <thisdjay@gmail.com>
2018-06-14 11:15:56 -0700

    Removed commented code for testing first and last name

--- test/test-users.js
+++ test/test-users.js
@@ -146,60 +146,6 @@ describe('/api/user', function() {
             expect(res.body.location).to.equal('password');
           });
       });
-      // it('Should reject users with non-string first name', function() {
-      //   return chai
-      //     .request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       username,
-      //       password,
-      //       firstName: 1234,
-      //       lastName
-      //     })
-      //     .then(() =>
-      //       expect.fail(null, null, 'Request should not succeed')
-      //     )
-      //     .catch(err => {
-      //       if (err instanceof chai.AssertionError) {
-      //         throw err;
-      //       }
-
-      //       const res = err.response;
-      //       expect(res).to.have.status(422);
-      //       expect(res.body.reason).to.equal('ValidationError');
-      //       expect(res.body.message).to.equal(
-      //         'Incorrect field type: expected string'
-      //       );
-      //       expect(res.body.location).to.equal('firstName');
-      //     });
-      // });
-      // it('Should reject users with non-string last name', function() {
-      //   return chai
-      //     .request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       username,
-      //       password,
-      //       firstName,
-      //       lastName: 1234
-      //     })
-      //     .then(() =>
-      //       expect.fail(null, null, 'Request should not succeed')
-      //     )
-      //     .catch(err => {
-      //       if (err instanceof chai.AssertionError) {
-      //         throw err;
-      //       }
-
-      //       const res = err.response;
-      //       expect(res).to.have.status(422);
-      //       expect(res.body.reason).to.equal('ValidationError');
-      //       expect(res.body.message).to.equal(
-      //         'Incorrect field type: expected string'
-      //       );
-      //       expect(res.body.location).to.equal('lastName');
-      //     });
-      // });
       // it('Should reject users with non-trimmed username', function() {
       //   return chai
       //     .request(app)
