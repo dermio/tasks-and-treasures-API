8d9bae17ac98ad2203ec6d6f308d6c96120a70b9
dermio <thisdjay@gmail.com>
2018-06-14 11:17:51 -0700

    Removed test for trimming first and last name

--- test/test-users.js
+++ test/test-users.js
@@ -350,37 +350,6 @@ describe('/api/user', function() {
       //       expect(passwordIsCorrect).to.be.true;
       //     });
       // });
-      // it('Should trim firstName and lastName', function() {
-      //   return chai
-      //     .request(app)
-      //     .post('/api/users')
-      //     .send({
-      //       username,
-      //       password,
-      //       firstName: ` ${firstName} `,
-      //       lastName: ` ${lastName} `
-      //     })
-      //     .then(res => {
-      //       expect(res).to.have.status(201);
-      //       expect(res.body).to.be.an('object');
-      //       expect(res.body).to.have.keys(
-      //         'username',
-      //         'firstName',
-      //         'lastName'
-      //       );
-      //       expect(res.body.username).to.equal(username);
-      //       expect(res.body.firstName).to.equal(firstName);
-      //       expect(res.body.lastName).to.equal(lastName);
-      //       return User.findOne({
-      //         username
-      //       });
-      //     })
-      //     .then(user => {
-      //       expect(user).to.not.be.null;
-      //       expect(user.firstName).to.equal(firstName);
-      //       expect(user.lastName).to.equal(lastName);
-      //     });
-      // });
     });
 
     // describe('GET', function() {
