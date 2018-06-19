5545684f80aba0dc3effa3dc558b2f5d47b377bf
dermio <thisdjay@gmail.com>
2018-06-19 12:23:45 -0700

    Added value for CLIENT_ORIGIN

--- config.js
+++ config.js
@@ -8,3 +8,5 @@ exports.PORT = process.env.PORT || 8080;
 // Auth
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
+
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000";
