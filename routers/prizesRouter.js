a7cd3a16eba3c3ae276bdaf117bedec4f93626da
dermio <thisdjay@gmail.com>
2018-08-21 15:33:18 -0700

    fix #3

--- routers/prizesRouter.js
+++ routers/prizesRouter.js
@@ -43,15 +43,20 @@ router.post("/", jsonParser, jwtAuth, (req, res) => {
     }
   }
 
-  Prize.create({
-        prizeName: req.body.prizeName,
-        familyCode: req.body.familyCode
-      })
-      .then(prize => res.status(201).json(prize.serialize()))
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ message: "Internal server error" });
-      });
+  Prize.deleteMany({
+    familyCode: req.body.familyCode
+  })
+  .then(() => 
+    Prize.create({
+      prizeName: req.body.prizeName,
+      familyCode: req.body.familyCode
+    })
+    .then(prize => res.status(201).json(prize.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    })
+  );
 });
 
 // DELETE prize, for Parent User
