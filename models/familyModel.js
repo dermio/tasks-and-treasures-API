0c5a65d3e26e6fbe2a518e3b93f0561d348ec11f
dermio <thisdjay@gmail.com>
2018-12-04 20:25:52 -0800

    Created Family schema and model

---
+++ models/familyModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+require("./taskModel");
+
+const familySchema = mongoose.Schema({
+  familyCode: {
+    type: String,
+    required: true
+  },
+  tasksFinalized: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  currentTasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    default: null,
+    ref: "Task"
+  }]
+});
+
+const Family = mongoose.model("Family", familySchema);
+module.exports = { Family };
+
+
+/*
+Look up Tasks by familyCode:
+Get familyCode, find family with familyCode in familyModel,
+then return the current task(s)
+
+For that family => currentTask
+
+- When reset, empty out currentTasks
+- When Finalize, set `tasksFinalized` to True
+*/
